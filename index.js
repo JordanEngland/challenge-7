0dd84bad0cde8827aaa6652dc1122c6adb745a5d
JordanEngland <lcplengland@gmail.com>
2023-05-15 16:09:48 -0500

    added the packages needed fo the app

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -67,10 +70,10 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
