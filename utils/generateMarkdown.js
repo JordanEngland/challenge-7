3aea945fa803b6df81c06466e9b2ec2aed0e2e9a
JordanEngland <lcplengland@gmail.com>
2023-05-16 15:12:38 -0500

    added the function for having no license to return empty string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -32,7 +32,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
+
+This project is licensed under the ${license} license. Click [here](${renderLicenseLink(license)}) for more information.
+
+${renderLicenseBadge(license)}
+`;
+  } else {
+    return "";
+  }
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
