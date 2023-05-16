9fdfac3d12248b7b03940e7b8b6ed49ec3b306cd
JordanEngland <lcplengland@gmail.com>
2023-05-15 22:19:26 -0500

    created the markdown function for the readme

---
+++ docs/README.md
@@ -0,0 +1,38 @@
+# demo
+
+
+
+## Description
+
+demo
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+demo
+
+## Usage
+
+demo
+
+## Contributing
+
+demo
+
+## Tests
+
+demo
+
+undefined
+
+## Questions
+
+If you have any questions, please contact me at demo. You can also check out my [GitHub profile](https://github.com/demo).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Apache 2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "GPL 3.0") {
+    return "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "BSD 3") {
+    return "https://opensource.org/licenses/BSD-3-Clause";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -26,6 +38,42 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+
+If you have any questions, please contact me at ${data.email}. You can also check out my [GitHub profile](https://github.com/${data.github}).
 `;
 }
 
